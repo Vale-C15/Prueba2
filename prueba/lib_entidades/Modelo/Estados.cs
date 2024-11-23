a0c2b6aefba2815b76e73c4dd54a79129b1ee7d0
Valeria Cardona <valeriacardona1116177@correo.itm.edu.co>
2024-11-23 01:14:54 -0500

    creacion de entidades

---
+++ prueba/lib_entidades/Modelo/Estados.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace lib_entidades.Modelos
+{
+    public class Estados
+    {
+        [Key] public int Id { get; set; } // 1. Activo     2. Inactivo
+        public string? Nombre { get; set; }
+
+        public bool Validar()
+        {
+            if (string.IsNullOrEmpty(Nombre))
+            {
+                Console.WriteLine("Por favor, registra el estudiante para continuar");
+                return false;
+            }
+
+            return true;
+        }
+
+    }
+}
